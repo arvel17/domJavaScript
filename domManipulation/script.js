2c5e4d4e2a6fe031cf2f8b38d9c5760d67790a45
Arvel Kartawijaya <arvel.kartawijaya@binus.ac.id>
2024-02-23 18:24:32 +0700

    materi domManipulation

---
+++ domManipulation/script.js
@@ -0,0 +1,27 @@
+// //element.innerHTML
+// const h1 = document.getElementById('judul');
+// judul.innerHTML = "Belajar DOM Manipulation";
+
+// const sectionA = document.querySelector('a');
+// sectionA.innerHTML ="semua diganti";
+
+// // //element.style.<property>
+// const title = document.querySelector('#judul');
+// title.style.color = "yellow"; 
+
+// //element.setAttribute
+// const attribute = document.getElementById('a');
+// attribute.setAttribute('newAttribute', 'new');
+
+//class list
+//element.classList.add();
+const p2 = document.querySelector('.p2');
+p2.classList.add('label');
+
+//element.classList.remove();
+p2.classList.remove('label');
+
+//element.classList.toggle(); ->seperti lampu
+p2.classList.toggle('label');
+
+//element.classList.item(<ke-berapa>);
